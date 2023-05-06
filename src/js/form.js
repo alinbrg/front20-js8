8a544aebe2c5e2c2c0c1c7e9d1b118548df0fd79
Alina Bregvadze <alina.bregvadze@gmail.com>
2023-05-06 22:01:18 +0400

    final version

--- src/js/form.js
+++ src/js/form.js
@@ -33,6 +33,7 @@ const form = document.querySelector("#register-user"),
 	// user id ფორმში, რომელიც გვჭირდება დაედითებისთვის
 	user_id = document.querySelector("#user_id");
 let userGender = document.querySelector("[name='gender']");
+let userList = document.querySelector("tbody");
 
 const createUserUrl = "https://borjomi.loremipsum.ge/api/register", //method POST  ყველა ველი სავალდებულო
 	getAllUsersUrl = "https://borjomi.loremipsum.ge/api/all-users", //method GET
@@ -99,7 +100,26 @@ function renderUsers(usersArray) {
 	// TODO: usersArray არის სერვერიდან დაბრუნებული ობიექტების მასივი
 	// TODO: ამ მონაცმების მიხედვით html ში ჩასვით ცხრილი როგორც "ცხრილი.png" შია
 
-	console.log(usersArray);
+	// console.log(usersArray);
+	const userRows = usersArray.map((user) => {
+		return `
+					<tr>
+						<td>${user.id}</td>
+						<td>${user.first_name}</td>
+						<td>${user.last_name}</td>
+						<td>${user.email}</td>
+						<td>${user.id_number}</td>
+						<td>${user.phone}</td>
+						<td>${user.zip_code}</td>
+						<td>${user.gender}</td>
+						<td>
+								<button class="edit " type="button" data-user-id="${user.id}" data-name="satesto">Edit</button>
+								<button class="delete " type="button" data-user-id="${user.id}">Delete</button>
+						</td>
+					</tr>`;
+	});
+	// console.log(usersArray);
+	userList.innerHTML = userRows.join("");
 	userActions(); // ყოველ რენდერზე ახლიდან უნდა მივაბათ ივენთ ლისნერები
 }
 
@@ -110,32 +130,68 @@ function userActions() {
 	// 3. id შეინახეთ data-user-id ატრიბუტად ღილაკებზე, data ატრიბუტებზე წვდომა შეგიძლიათ dataset-ის გამოყენებით მაგ:selectedElement.dataset
 	// 4. წაშლა ღილაკზე დაჭერისას უნდა გაიგზავნოს წაშლის მოთხოვნა (deleteUser ფუნქციის მეშვეობით) სერვერზე და გადაეცეს id
 	// 5. ედიტის ღილაკზე უნდა გაიხსნას მოდალი სადაც ფორმი იქნება იმ მონაცემებით შევსებული რომელზეც მოხდა კლიკი. ედიტის ღილაკზე უნდა გამოიძახოთ getUser ფუნქცია და რომ დააბრუნებს ერთი მომხმარებლის დატას (ობიექტს და არა მასივს)  ეს დატა უნდა შეივსოს ფორმში და ამის შემდეგ შეგიძლიათ დააედიტოთ ეს ინფორმაცია. ფორმის დასაბმითებისას უნდა მოხდეს updateUser() ფუნქციის გამოძახება, სადაც გადასცემთ განახლებულ იუზერის ობიექტს, გვჭირდება იუზერის აიდიც, რომელიც  მოდალის გახსნისას user_id-ის (hidden input არის და ვიზუალურად არ ჩანს) value-ში შეგიძლიათ შეინახოთ
+
+	const editBtns = document.querySelectorAll(".edit");
+	const deleteBtns = document.querySelectorAll(".delete");
+
+	editBtns.forEach((btn) => {
+		btn.addEventListener("click", async (e) => {
+			// console.log(btn.dataset.userId, "edit");
+
+			// მივიღოთ ინფორმაცია იუზერის შესახებ
+			const data = await getUser(btn.dataset.userId);
+
+			// console.log(data);
+			const user = data.users;
+
+			// შევავსოთ ფორმა ამ იუზერის ინფორმაციით
+			userName.value = user.first_name;
+			userLastName.value = user.last_name;
+			userEmail.value = user.email;
+			userMobile.value = user.phone;
+			userPersonalId.value = user.id_number;
+			userZipCode.value = user.zip_code;
+			user_id.value = user.id;
+
+			// სქესის შესაბამისი ინფუთი მოვნიშნოთ
+			document
+				.querySelector(`input[value=${user.gender}]`)
+				.setAttribute("checked", true);
+
+			// გავსხნათ ფორმა
+			openRegModal.click();
+		});
+	});
+
+	deleteBtns.forEach((btn) => {
+		btn.addEventListener("click", (e) => {
+			// console.log(btn.dataset.userId, "delete");
+			const id = btn.dataset.userId;
+			deleteUser(id);
+		});
+	});
 }
 
 function getAllUsers() {
 	fetch("https://borjomi.loremipsum.ge/api/all-users")
 		.then((res) => res.json())
 		.then((data) => {
-			console.log(data.users);
+			// console.log(data.users);
 			// html-ში გამოტანა მონაცემების
-			renderUsers();
+
+			renderUsers(data.users);
 		})
 		.catch((err) => console.log(err));
 }
 
-function getUser(id) {
-	fetch(`https://borjomi.loremipsum.ge/api/get-user/${id}`, {
+async function getUser(id) {
+	const res = await fetch(`https://borjomi.loremipsum.ge/api/get-user/${id}`, {
 		method: "get",
-	})
-		.then((res) => res.json())
-		.then((data) => {
-			// გვიბრუნებს იუზერის ობიექტს
-			console.log(data);
-			//TODO: შენახვის, ედიტირების და წაშლის შემდეგ ახლიდან წამოიღეთ დატა
-		})
-		.catch((error) => {
-			console.log(error);
-		});
+	});
+	// გვიბრუნებს იუზერის ობიექტს
+	const data = await res.json();
+
+	return data;
 }
 
 function updateUser(userObj) {
@@ -144,6 +200,30 @@ function updateUser(userObj) {
 	//  method: "put",  https://borjomi.loremipsum.ge/api/update-user/${userObj.id}
 	// userObj -იც უნდა გადასცეთ როგორც addNewUser ფუნქიაში
 	// TODO: შენახვის, ედიტირების და წაშლის შემდეგ ახლიდან წამოიღეთ დატა
+
+	fetch(`https://borjomi.loremipsum.ge/api/update-user/${userObj.id}`, {
+		method: "put",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify(userObj),
+	})
+		.then((res) => res.json())
+		.then((data) => {
+			console.log(data);
+			getAllUsers();
+
+			// ფორმის რესეტი
+			form.reset();
+			user_id.value = "";
+			document.querySelector(`input[checked]`).removeAttribute("checked"); //მონიშნული სქესის გაუქმება
+
+			// მოდალის დახურვა
+			document.querySelector("#reg-form-modal").classList.remove("active");
+		})
+		.catch((e) => {
+			console.log("error", e);
+		});
 }
 
 getAllUsers();
@@ -169,4 +249,12 @@ form.addEventListener("submit", (e) => {
 
 	// თუ დაედითებას ვაკეთებთ, ჩვენ ვანიჭებთ მნიშვნელობას userActions ფუნქციაში
 	// TODO: თუ user_id.value არის (არაა ცარიელი სტრინგი) მაშინ უნდა დავაედიტოთ, (როცა ფორმს ედითის ღილაკის შემდეგ იუზერის ინფუთით ვავსებთ, ვაედითებთ და ვასაბმითებთ) -->  updateUser(userObj);
+
+	if (userObj.id) {
+		// console.log("update");
+		updateUser(userObj);
+	} else {
+		// console.log("add new");
+		addNewUser(userObj);
+	}
 });
